2ff3a199488f4b259b1b34a0cd3c359e797d4442
SamuelLorrain <samuel.lorrain@protonmail.com>
2020-05-16 01:42:10 +0200

    Add scroll

--- public/touchpad_2.js
+++ public/touchpad_2.js
@@ -11,7 +11,6 @@ var mouseY = 0;
 function calcDistance(){
     moveDistancanvase = Math.sqrt(Math.pow((mouseY - canvas.height/2),2) + Math.pow((mouseX - canvas.width/2),2));
     moveAngle = Math.atan((mouseY - canvas.height/2) / (mouseX - canvas.width/2)) * (180/Math.PI);
-
     if(moveAngle === -90){
         moveAngle = 0;
     }
@@ -32,7 +31,7 @@ function calcDistance(){
 canvas.addEventListener('touchstart', function(evt){
     evt.preventDefault();
     if(evt.touches.length === 1){ // left_click
-        canvas.ontouchmove = function(evtMove){
+        canvas.ontouchmove = function(evtMove){ //move
             evtMove.preventDefault();
             isMoving = true;
             mouseX = evtMove.touches[0].clientX;
@@ -48,21 +47,39 @@ canvas.addEventListener('touchstart', function(evt){
             if(evtEnd.touches.length === 0 && !isMoving){
                 socket.emit('message', 'left_click');
                 socket.emit('mouse_click', 'LEFT_CLICK');
+                isMoving = false;
             }
-            isMoving = false;
+            if(evtEnd.touches.length === 0){
+                isMoving = false;
+            }
         };
     }
     else if(evt.touches.length === 2){ // right_click
+        canvas.ontouchmove = function(evtScroll){ //scroll
+            evtScroll.preventDefault();
+            isMoving = true;
+            mouseX = evtScroll.touches[0].clientX;
+            mouseY = evtScroll.touches[0].clientY;
+            calcDistance();
+            if(moveAngle < 170){
+                socket.emit('mouse_click', 'SCROLL_UP');
+            } else{
+                socket.emit('mouse_click', 'SCROLL_DOWN');
+            }
+        }
         canvas.ontouchend = function(evtEnd){
             evtEnd.preventDefault();
             if(evtEnd.touches.length === 0 && !isMoving){
                 socket.emit('message', 'right_click');
                 socket.emit('mouse_click', 'RIGHT_CLICK');
             }
-            isMoving = false;
+            if(evtEnd.touches.length === 0){
+                isMoving = false;
+            }
         };
     }
     else{
         canvas.ontouchend = null;
+        canvas.ontouchmove = null;
     }
 });
