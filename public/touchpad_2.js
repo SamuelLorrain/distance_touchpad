e835e1ad549d3ecf9db3c41c7b96daefec6cb4b8
SamuelLorrain <samuel.lorrain@protonmail.com>
2020-05-16 00:58:21 +0200

    right_click and left_click works properly

--- public/touchpad_2.js
+++ public/touchpad_2.js
@@ -2,21 +2,32 @@ var socket = io();
 var canvas = document.querySelector('#mouse-canvas');
 var debug = document.querySelector('.debug');
 
+var isMoving = false;
+
 canvas.addEventListener('touchstart', function(evt){
-    if(evt.touches.length === 1){ //single click
+    evt.preventDefault();
+    if(evt.touches.length === 1){ // left_click
+        canvas.ontouchmove = function(evtMove){
+            evtMove.preventDefault();
+            isMoving = true;
+        }
         canvas.ontouchend = function(evtEnd){
-            if(evtEnd.touches.length === 0){
+            evtEnd.preventDefault();
+            if(evtEnd.touches.length === 0 && !isMoving){
                 socket.emit('message', 'left_click');
                 socket.emit('mouse_click', 'LEFT_CLICK');
             }
+            isMoving = false;
         };
     }
-    else if(evt.touches.length === 2){ //secondary
+    else if(evt.touches.length === 2){ // right_click
         canvas.ontouchend = function(evtEnd){
-            if(evtEnd.touches.length === 0){
+            evtEnd.preventDefault();
+            if(evtEnd.touches.length === 0 && !isMoving){
                 socket.emit('message', 'right_click');
                 socket.emit('mouse_click', 'RIGHT_CLICK');
             }
+            isMoving = false;
         };
     }
     else{
